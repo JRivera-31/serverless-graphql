12fb8a995d1dcf31431801e7f266d1f4cf8e7eb6
Josh R <jriver35@uncc.edu>
2020-07-18 22:40:32 -0400

    users can now add todos to the public to do list, updates in real time

--- components/OnlineUsers/OnlineUsersWrapper.js
+++ components/OnlineUsers/OnlineUsersWrapper.js
@@ -34,35 +34,48 @@ const OnlineUsersWrapper = () => {
     })
   }
 
+  const GET_ONLINE_USERS = gql`
+  subscription getOnlineUsers {
+    online_users(order_by: {user: {name: asc}}) {
+      id
+      user {
+        name
+      }
+    }
+  }`
+
   const { loading, error, data } = useSubscription(
     gql`
       subscription getOnlineUsers {
-        online_users(order_by: {user: {name: asc}}) {
+        online_users(order_by: { user: { name: asc } }) {
           id
           user {
             name
           }
         }
-      }`
-  )
-  
+      }
+    `
+  );
+
   if (loading) {
-    return <span>Loading...</span>
+    return <span>Loading...</span>;
   }
   if (error) {
-    console.log(error)
-    return <span>Error!</span>
+    console.error(error);
+    return <span>Error!</span>;
   }
   if (data) {
     onlineUsersList = data.online_users.map(u => (
       <OnlineUser key={u.id} user={u.user} />
-    ))
+    ));
   }
 
   return (
     <div className="onlineUsersWrapper">
       <Fragment>
-        <div className="sliderHeader">Online users - {onlineUsersList.length}</div>
+        <div className="sliderHeader">
+          Online users - {onlineUsersList.length}
+        </div>
         {onlineUsersList}
       </Fragment>
     </div>
